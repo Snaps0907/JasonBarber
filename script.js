451d737388eeb82a6edb761925779ef75260def9
Snaps0907 <57373253+Snaps0907@users.noreply.github.com>
2020-07-05 19:10:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,24 @@
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+// console.log(toggleButton,navbarLinks)
 
-
-toggleButton.addEventListener('click', () => {
+toggleButton.addEventListener('click', ()=>  {
   navbarLinks.classList.toggle('active');
   toggleButton.classList.toggle('active');
 })
+
 
 function hide(){
   navbarLinks.className='navbar-links';
   toggleButton.className='toggle-button';
 }
 
+
 function validation(){
-  var form= document.getElementById("form");
-  var email= document.getElementById("email").value;
-  var text=document.getElementById("text");
-  var pattern = /^[^ ]+@[^ ]+\.[a-z]{1,2}$/;
+  let email= document.getElementById("email").value;
+  let text=document.getElementById("text");
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
   if (email.match(pattern)){
 
     text.innerHTML="Poprawny adres email";
@@ -32,3 +33,20 @@ function validation(){
     text.innerHTML=" "
   }
 }
+
+
+function verified(){
+  let textarea=document.getElementById("textarea").value
+  let email= document.getElementById("email").value
+  let text= document.getElementById("text").value
+
+  if(textarea=="" || email=="" || text==""){
+    alert("wypełnij wszystkie pola formularza")
+  }else{
+    alert("wiadomość została wysłana")
+  }
+
+
+}
+
+
